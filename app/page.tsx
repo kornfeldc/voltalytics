f642549ad18ef308ef5e11eb2fb0df810e2d89bd
kornfeldc <kornfeld.c@gmail.com>
2023-04-02 13:48:49 +0200

    Set dashboard as default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 "use client";
-import {Inter} from 'next/font/google'
-import {useRouter} from "next/navigation";
-
-const inter = Inter({subsets: ['latin']})
+import DashboardPage from "@/app/dashboard/page";
 
 export default function Home() {
-
-    const router = useRouter();
-    router.push("/dashboard");
-
     return (
-        <main></main>
+        <DashboardPage></DashboardPage>
     )
 }
