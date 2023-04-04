42561e8f65399abd3e4fb5cd5b5a84f82b18e371
kornfeldc <kornfeld.c@gmail.com>
2023-04-04 18:42:50 +0200

    Cache awattar data in localstorage

--- app/classes/awattarApi.ts
+++ app/classes/awattarApi.ts
@@ -4,18 +4,41 @@ import {ST} from "next/dist/shared/lib/utils";
 
 export class AwattarApi {
     static async getData({ hours = 5, offsetHours = 2 } = {}): Promise<Array<AwattarEntry> | null> {
-        const startMoment = moment().add(offsetHours*-1,'hours');
+        const startMoment = moment().startOf("hour").add(offsetHours*-1,'hours');
         const start = (startMoment.unix()*1000);
+
+        var key = `awd_${start}`;
+        
         let url = `https://api.awattar.at/v1/marketdata?start=${start}`;
         if(hours > 0) {
             const end = startMoment.add(hours, 'hours').unix()*1000; 
-            url += "&end="+end;
+            url += `&end=${end}`;
+            key += `_${end}`;
         }
+       
+        const storedData = this.getDataFromStorage(key);
+        if(storedData?.length > 0)
+            return storedData;
         
         const res = await fetch(url);
-        if (res.ok) 
-            return AwattarApi.parseResponse(await res.json());
+        if (res.ok) {
+            const data =  AwattarApi.parseResponse(await res.json());
+            this.saveDataToStorate(key, data);
+            return data;
+        }
+            
         return [];
+    }
+    
+    static saveDataToStorate(key: string, data: Array<AwattarEntry>) {
+        localStorage.setItem(key, JSON.stringify(data));
+    }
+    
+    static getDataFromStorage(key: string): Array<AwattarEntry> {
+        const content = localStorage.getItem(key);
+        if(!content)
+            return [];
+        return JSON.parse(content) as Array<AwattarEntry>;
     }
 
     static parseResponse(response: any): Array<AwattarEntry> {
