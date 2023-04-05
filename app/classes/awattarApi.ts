36222290246d82fdf9ac0c05deb21b146d5fbdb7
kornfeldc <kornfeld.c@gmail.com>
2023-04-05 08:08:19 +0200

    Clean up

--- app/classes/awattarApi.ts
+++ app/classes/awattarApi.ts
@@ -1,42 +1,41 @@
 import {AwattarEntry} from "@/app/interfaces";
-import moment, {max} from "moment";
-import {ST} from "next/dist/shared/lib/utils";
+import moment from "moment";
 
 export class AwattarApi {
-    static async getData({ hours = 5, offsetHours = 1 } = {}): Promise<Array<AwattarEntry> | null> {
-        const startMoment = moment().startOf("hour").add(offsetHours*-1,'hours');
-        const start = (startMoment.unix()*1000);
+    static async getData({hours = 5, offsetHours = 1} = {}): Promise<Array<AwattarEntry> | null> {
+        const startMoment = moment().startOf("hour").add(offsetHours * -1, 'hours');
+        const start = (startMoment.unix() * 1000);
 
         let key = `awd_${start}`;
-        
+
         let url = `https://api.awattar.at/v1/marketdata?start=${start}`;
-        if(hours > 0) {
-            const end = startMoment.add(hours, 'hours').unix()*1000; 
+        if (hours > 0) {
+            const end = startMoment.add(hours, 'hours').unix() * 1000;
             url += `&end=${end}`;
             key += `_${end}`;
         }
-       
+
         const storedData = this.getDataFromStorage(key);
-        if(storedData?.length > 0)
+        if (storedData?.length > 0)
             return storedData;
-        
+
         const res = await fetch(url);
         if (res.ok) {
-            const data =  AwattarApi.parseResponse(await res.json());
+            const data = AwattarApi.parseResponse(await res.json());
             this.saveDataToStorate(key, data);
             return data;
         }
-            
+
         return [];
     }
-    
+
     static saveDataToStorate(key: string, data: Array<AwattarEntry>) {
         localStorage.setItem(key, JSON.stringify(data));
     }
-    
+
     static getDataFromStorage(key: string): Array<AwattarEntry> {
         const content = localStorage.getItem(key);
-        if(!content)
+        if (!content)
             return [];
         return JSON.parse(content) as Array<AwattarEntry>;
     }
