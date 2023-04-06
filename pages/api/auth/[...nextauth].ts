9e28b548eb13784db196f0cd29b4f15f2a741395
kornfeldc <kornfeld.c@gmail.com>
2023-04-06 20:44:21 +0200

    Change secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,5 +8,5 @@ export default NextAuth({
             clientSecret:process.env.GOOGLE_AUTH_CLIENT_SECRET!
         })
     ],
-    secret: process.env.SECRET
+    secret: process.env.NEXT_PUBLIC_SECRET
 });
