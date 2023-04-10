c8e3175df1c4bd9de9555ba706a61e04e0ed2170
kornfeldc <kornfeld.c@gmail.com>
2023-04-10 09:55:20 +0200

    Change realtime card to diagram with animations

---
+++ app/components/solarMan/solarManLiveDiagram.tsx
@@ -0,0 +1,228 @@
+import React from "react";
+import {ISolarManRealTimeInfo} from "@/app/classes/solarManApi";
+import {Battery100Icon, BoltIcon, HomeIcon, LightBulbIcon, SunIcon} from "@heroicons/react/24/outline";
+import smStyles from "./solarMan.module.css";
+import globalStyles from "@/app/globals.module.css";
+
+interface SolarManLiveDiagramProps {
+    realTimeData: ISolarManRealTimeInfo;
+}
+
+export default function SolarManLiveDiagram({realTimeData}: SolarManLiveDiagramProps) {
+
+    const prepareValue = (watt: number | null | undefined): number => {
+        if (!watt) return 0;
+        let kw = watt / 1000;
+        if (Math.abs(kw) <= 0.03) kw = 0;
+        return kw;
+    }
+    
+    const format = (watt: number | null | undefined) => {
+        const kw = prepareValue(watt);
+        return `${kw.toLocaleString(undefined, {maximumFractionDigits: 2, minimumFractionDigits: 2})}`;
+    }
+    
+    const renderIcon = (icon: any, color: string, size = "10") => {
+        return (
+            <div className={`w-${size} h-${size} ${color}`}>
+                {icon}
+            </div>
+        );
+    }
+    
+    const renderLabel = (label: string) => <div className={`${globalStyles.label} whitespace-nowrap`}>{label}</div>;
+
+    const renderRealTimeStat = (
+        color: string,
+        value: number | undefined | null,
+        label: string,
+        right = false,
+        additionalRenderFunction: JSX.Element | undefined = undefined) => {
+
+        return (
+            <div className={right ? "text-right" : ""}>
+                <div className={[
+                    value ? color : "text-inactive",
+                    smStyles.stat,
+                    right ? smStyles.statRight : ""
+                ].join(" ")}>
+                    <span className={!right ? "mr-2" : ""}>{format(value)}&nbsp;<span
+                        className="text-xs">kWh</span></span>
+                    {additionalRenderFunction && additionalRenderFunction}
+                </div>
+                {renderLabel(label)}
+            </div>
+        );
+    }
+    
+    const renderCorner = (
+        label: string,
+        color: string,
+        icon: any,
+        value: number,
+        right = false
+    ) => {
+        if(prepareValue(value) === 0)
+            color = "text-inactive";
+        
+        return (
+            <div className={["flex", right ? "text-right justify-end" : ""].join(" ")}>
+                {right && renderRealTimeStat(color, value, label, right)}
+                {renderIcon(icon, color)}
+                {!right && renderRealTimeStat(color, value, label, right)}
+            </div>
+        );
+    }
+
+    const renderProduction = () => {
+        return renderCorner("Production", "text-neutral", <SunIcon/>, realTimeData.generationPower ?? 0);
+    }
+
+    const renderGrid = () => {
+        if(prepareValue(realTimeData.purchasePower)) 
+            return renderCorner("From Grid", "text-negative", <BoltIcon/>, realTimeData.purchasePower ?? 0, true);
+        if(prepareValue(realTimeData.gridPower))
+            return renderCorner("To Grid", "text-positive", <BoltIcon/>, realTimeData.gridPower ?? 0, true);
+        return renderCorner("Grid", "", <BoltIcon/>, 0, true);
+    }
+
+    const getBatteryColor = (percentage: number) => (
+        percentage >= 80 ? smStyles.batteryFull :
+            percentage >= 50 ? smStyles.batteryMedium :
+                percentage >= 25 ? smStyles.batteryLow :
+                    smStyles.batteryEmpty
+    );
+
+    const renderBatteryIcon = () => {
+        if (!realTimeData) return;
+        return (
+            <span className={[
+                "mr-1",
+                smStyles.battery,
+                getBatteryColor(realTimeData.batterySoc ?? 0)
+            ].join(" ")}>{realTimeData.batterySoc}%</span>
+        );
+    }
+    
+    const renderBattery = () => {
+        if(prepareValue(realTimeData.chargePower))
+            return renderCorner("Charging Battery", "text-positive", renderBatteryIcon(), realTimeData.chargePower ?? 0);
+        if(prepareValue(realTimeData.dischargePower))
+            return renderCorner("Discharging Battery", "text-neutral2", renderBatteryIcon(), realTimeData.dischargePower ?? 0);
+        return renderCorner("Battery", "", renderBatteryIcon(), 0, true);
+    }
+
+    const renderUsage = () => {
+        return renderCorner("Usage", "text-warning", <LightBulbIcon/>, realTimeData.usePower ?? 0, true);
+    }
+
+    const renderHouse = () => {
+        return (
+            <div className={"flex justify-center items-center"}>
+                {renderIcon(<HomeIcon></HomeIcon>, "text-inactive", "12")}
+            </div>
+        );
+    }
+
+    const renderFlow = (
+        color: string,
+        topClass: string,
+        bottomClass: string,
+        topDot: string,
+        bottomDot: string) => {
+        return (<div className="flex h-5">
+            <div className="relative w-full">
+                <div className={["line absolute", topClass, color].join(" ")}>
+                    <div className={["dot z-10 w-2 h-2 bg-amber-300 absolute rounded-full", topDot].join(" ")}></div>
+                </div>
+                <div className={["line absolute", bottomClass, color].join(" ")}>
+                    <div className={["dot z-10 w-2 h-2 bg-amber-300 absolute rounded-full", bottomDot].join(" ")}></div>
+                </div>
+            </div>
+        </div>);
+    }
+
+    const renderProductionFlow = () => {
+
+        const animate =
+            prepareValue(realTimeData.generationPower ?? 0) > 0 
+                ? smStyles.animateLeftToRight 
+                : "hidden";
+
+        return renderFlow(
+            "bg-slate-500",
+            "left-[18px] w-1 h-4",
+            "top-4 left-[18px] w-full h-1",
+            "hidden",
+            "top-[-2px] left-[-2px] " + animate
+        );
+    }
+
+    const renderGridFlow = () => {
+
+        const animate =
+            prepareValue(realTimeData.purchasePower ?? 0) > 0
+                ? smStyles.animateRightToLeft 
+                : " hidden";
+
+        return renderFlow(
+            "bg-slate-500",
+            "right-[18px] w-1 h-4",
+            "top-4 right-[18px] w-full h-1",
+            "hidden",
+            "top-[-2px] right-[-2px] " + animate,
+        );
+    }
+
+    const renderBatteryFlow = () => {
+
+        const animate =
+            Math.abs(realTimeData.chargePower ?? 0) > 0
+                ? smStyles.animateRightToLeft
+                : Math.abs(realTimeData.dischargePower ?? 0) > 0
+                    ? smStyles.animateLeftToRight 
+                    : "hidden";
+
+        return renderFlow(
+            "bg-slate-500",
+            "top-2 left-[18px] w-full h-1",
+            "top-2 left-[18px] w-1 h-5",
+            "top-[-2px] left-[-2px] " + animate,
+            "hidden"
+        );
+    }
+
+    const renderUsageFlow = () => {
+
+        const animate = prepareValue(realTimeData.usePower) 
+            ? smStyles.animateLeftToRight 
+            : "hidden";
+
+        return renderFlow(
+            "bg-slate-500",
+            "top-2 right-[18px] w-full h-1",
+            "top-2 right-[18px] w-1 h-5 pb-5",
+            "top-[-2px] right-[-2px] " + animate,
+            "hidden"
+        );
+    }
+
+    return (
+        <div>
+            <div className={"grid grid-cols-3"}>
+                <div>{renderProduction()}</div>
+                <div></div>
+                <div>{renderGrid()}</div>
+
+                <div>{renderProductionFlow()}</div>
+                <div className={"row-span-2 flex align-middle justify-center"}>{renderHouse()}</div>
+                <div>{renderGridFlow()}</div>
+                <div>{renderBatteryFlow()}</div>
+                <div>{renderUsageFlow()}</div>
+
+                <div className={"mt-3"}>{renderBattery()}</div>
+                <div></div>
+                <div className={"mt-3"}>{renderUsage()}</div>
+            </div>
+        </div>);
+}
--- app/components/solarMan/solarManRealTimeCard.tsx
+++ app/components/solarMan/solarManRealTimeCard.tsx
@@ -4,8 +4,7 @@ import {ISolarManRealTimeInfo, SolarManApi} from "@/app/classes/solarManApi";
 import {IUser} from "@/app/classes/db";
 import moment from "moment";
 import LoadingSpinner from "@/app/components/loadingSpinner";
-import smStyles from "./solarMan.module.css";
-import globalStyles from "../../globals.module.css";
+import SolarManLiveDiagram from "@/app/components/solarMan/solarManLiveDiagram";
 
 interface SolarManCardProps {
     user: IUser;
@@ -24,130 +23,21 @@ export default function SolarManRealTimeCard({user}: SolarManCardProps) {
         })
     }, [user]);
 
-    const prepareValue = (watt: number | null | undefined): number => {
-        if (!watt) return 0;
-        let kw = watt / 1000;
-        if (Math.abs(kw) <= 0.03) kw = 0;
-        return kw;
-    }
-
-    const format = (watt: number | null | undefined) => {
-        const kw = prepareValue(watt);
-        return `${kw.toLocaleString(undefined, {maximumFractionDigits: 2, minimumFractionDigits: 2})}`;
-    }
-
     const getLastUpdateTime = (lastUpdateTime: number | undefined | null): string => {
         if (!lastUpdateTime) return "";
         return moment(lastUpdateTime * 1000).format("DD.MM.YYYY HH:mm");
-    }
-
-    const renderLabel = (label: string) => <div className={globalStyles.label}>{label}</div>;
-
-    const renderRealTimeStat = (
-        color: string,
-        value: number | undefined | null,
-        label: string,
-        right = false,
-        additionalRenderFunction: JSX.Element | undefined = undefined) => {
-
-        return (
-            <div className={right ? "text-right" : ""}>
-                <div className={[
-                    value ? color : "text-inactive",
-                    smStyles.stat,
-                    right ? smStyles.statRight : ""
-                ].join(" ")}>
-                    <span className={!right ? "mr-2" : ""}>{format(value)}&nbsp;<span className="text-xs">kWh</span></span>
-                    {additionalRenderFunction && additionalRenderFunction}
-                </div>
-                {renderLabel(label)}
-            </div>
-        );
-    }
-
-    const renderProduction = (right = false) => {
-        if (!realTimeData) return;
-        return renderRealTimeStat("text-neutral", realTimeData.generationPower, "Production", right);
-    }
-
-    const renderGrid = (right = false) => {
-        if (!realTimeData) return;
-        if (prepareValue(realTimeData.purchasePower))
-            return renderRealTimeStat(
-                "text-negative",
-                Math.abs(realTimeData.purchasePower ?? 0),
-                "From Grid",
-                right);
-        if (prepareValue(realTimeData.gridPower))
-            return renderRealTimeStat(
-                "text-positive",
-                realTimeData.gridPower,
-                "To Grid",
-                right);
-        return renderRealTimeStat("", 0, "Grid", true);
-    }
-
-    const getBatteryColor = (percentage: number) => (
-        percentage >= 80 ? smStyles.batteryFull :
-            percentage >= 50 ? smStyles.batteryMedium :
-                percentage >= 25 ? smStyles.batteryLow :
-                    smStyles.batteryEmpty
-    );
-
-    const renderBatteryIcon = () => {
-        if (!realTimeData) return;
-        return (
-            <span className={[
-                smStyles.battery,
-                getBatteryColor(realTimeData.batterySoc ?? 0)
-            ].join(" ")}>{realTimeData.batterySoc} %</span>
-        );
-    }
-
-    const renderBattery = (right = false) => {
-        if (!realTimeData) return;
-        if (prepareValue(realTimeData.dischargePower))
-            return renderRealTimeStat(
-                "text-neutral2",
-                Math.abs(realTimeData.dischargePower ?? 0),
-                "Discharging Battery",
-                right,
-                renderBatteryIcon()
-            );
-        if (prepareValue(realTimeData.chargePower))
-            return renderRealTimeStat(
-                "text-positive",
-                Math.abs(realTimeData.chargePower ?? 0),
-                "Charging Battery",
-                right,
-                renderBatteryIcon());
-        return (<div>
-            <div>{renderBatteryIcon()}</div>
-            {renderLabel("Battery idle")}
-        </div>);
-    }
-
-    const renderUsage = (right = false) => {
-        if (!realTimeData) return;
-        return renderRealTimeStat("text-warning", realTimeData.usePower, "Usage", right);
     }
 
     const renderRealtimeContent = () => {
         if (!realTimeData) return null;
 
         return (
-            <div className={smStyles.grid}>
-                <h2>{getLastUpdateTime(realTimeData.lastUpdateTime)}</h2>
-                <div>
-                    <div>{renderProduction()}</div>
-                    <div>{renderGrid(true)}</div>
-                </div>
-                <div>
-                    <div>{renderBattery()}</div>
-                    <div>{renderUsage(true)}</div>
-                </div>
+            <div>
+                <h2 className={"mb-2"}>{getLastUpdateTime(realTimeData.lastUpdateTime)}</h2>
+                <SolarManLiveDiagram realTimeData={realTimeData}></SolarManLiveDiagram>
             </div>
         );
+
     }
 
     const renderLoadingOrData = () => {
