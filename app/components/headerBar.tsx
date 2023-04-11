15799018a1caf483ca73e471bdfec2e66bffcae0
kornfeldc <kornfeld.c@gmail.com>
2023-04-11 20:28:02 +0200

    Add google profile picture

--- app/components/headerBar.tsx
+++ app/components/headerBar.tsx
@@ -1,27 +1,40 @@
+"use client";
 import React from "react";
-import {BoltIcon} from "@heroicons/react/24/outline";
-import {UserCircleIcon} from "@heroicons/react/24/outline";
+import {BoltIcon, UserCircleIcon} from "@heroicons/react/24/outline";
 import Link from "next/link";
+import {useSession} from "next-auth/react";
 
 export default function HeaderBar() {
 
-    const { version } = require('@/package.json');
-    
+    const {version} = require('@/package.json');
+    const {data: session} = useSession();
+    // const session = {} as any;
+
     return (
         <div className="flex bg-slate-50 shadow-md shadow-slate-300
             dark:bg-slate-950 dark:shadow-slate-900 dark:text-slate-300">
-            <Link 
+            <Link
                 href="/dashboard"
-                className="grow pl-2  flex content-center
+                className="grow pl-2 flex content-center justify-center
                 ">
-                <BoltIcon className="p-2 h-12 w-12 text-amber-400"></BoltIcon>
+                <BoltIcon className="p-2 pt-3 h-12 w-12 text-amber-400"></BoltIcon>
                 <h1 className="text-2xl self-center text-slate-700 dark:text-slate-300">
                     <span className="tracking-widest">voltalytics</span>
                     <span className="ml-2 text-xs font-thin">{version}</span>
                 </h1>
             </Link>
             <Link href="/user">
-                <UserCircleIcon className="p-2 mr-3 h-12 w-12 text-amber-400"></UserCircleIcon>
+                {session?.user?.image &&
+                    (
+                        <img
+                            className={"w-8 h-8 rounded-full mr-4 my-2 bg-amber-400 border-2 border-amber-400"}
+                            src={session.user.image}
+                            alt={session.user.name!}/>
+                    )
+                }
+                {!session?.user?.image &&
+                    <UserCircleIcon className="p-2 mr-3 h-12 w-12 text-amber-400"></UserCircleIcon>
+                }
             </Link>
         </div>
     );
