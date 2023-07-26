962b469a14224c747255c97343c1370784edd9b8
kornfeldc <kornfeld.c@gmail.com>
2023-07-26 17:35:21 +0200

    Remove debug alerts

--- app/classes/awattarApi.ts
+++ app/classes/awattarApi.ts
@@ -6,35 +6,30 @@ export class AwattarApi {
     static async getData({hours = 5, offsetHours = 1} = {}): Promise<Array<AwattarEntry> | null> {
 
         try {
-        const startMoment = moment().startOf("hour").add(-12, 'hours');
-        const start = (startMoment.unix() * 1000);
-        
-        alert("start getting from cache");
-        let data: Array<AwattarEntry> = await VoltCache.get(
-            `aWATTarData_${start}`,
-            "",
-            60 * 60 /*1 hour*/,
-            async (): Promise<any> => {
-                try {
-                let url = `https://api.awattar.at/v1/marketdata?start=${start}`;
-                const res = await fetch(url);
-                if (res.ok)
-                    return AwattarApi.parseResponse(await res.json());
-                return [];
+            const startMoment = moment().startOf("hour").add(-12, 'hours');
+            const start = (startMoment.unix() * 1000);
+
+            let data: Array<AwattarEntry> = await VoltCache.get(
+                `aWATTarData_${start}`,
+                "",
+                60 * 60 /*1 hour*/,
+                async (): Promise<any> => {
+                    try {
+                        let url = `https://api.awattar.at/v1/marketdata?start=${start}`;
+                        const res = await fetch(url);
+                        if (res.ok)
+                            return AwattarApi.parseResponse(await res.json());
+                        return [];
+                    } catch (e: any) {
+                        return [];
+                    }
                 }
-                catch(e: any) {
-                    alert(e.description);
-                    return [];
-                }
-            }
-        )
-        data = data.filter(entry => this.showEntry(entry, hours, offsetHours));
-        return data;
-        }
-        catch(e1: any) {
-            alert("e1 "+e1.description);
+            )
+            data = data.filter(entry => this.showEntry(entry, hours, offsetHours));
+            return data;
+        } catch (e1: any) {
             return [];
-        } 
+        }
     }
 
     static showEntry(entry: AwattarEntry, hours: number, offsetHours: number): boolean {
